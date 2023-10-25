3fdbdbe2d927051fd60f1d96648cac80e447b1bc
sanlaulecasagame <121693318+sanlaulecasagame@users.noreply.github.com>
2023-10-24 21:07:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var welcomeMessage = document.querySelector("h1");
+    welcomeMessage.textContent = "¡Bienvenido a mi página web!";
+});
