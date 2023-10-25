2c85e7fc3fe431dabc0a26a84cc7c2ed70030831
sanlaulecasagame <121693318+sanlaulecasagame@users.noreply.github.com>
2023-10-24 21:15:43 -0300

    styles.cs

--- styles.cs
+++ styles.cs
@@ -1,8 +1,37 @@
 /* styles.css */
 body {
-    background-color: #000000;
     font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f0f0f0;
 }
-h1 {
-    color: #333;
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+}
+nav ul {
+    list-style: none;
+    padding: 0;
+    text-align: center;
+}
+nav li {
+    display: inline;
+    margin: 0 15px;
+}
+nav a {
+    text-decoration: none;
+}
+main {
+    max-width: 800px;
+    margin: 20px auto;
+    padding: 20px;
+    background-color: #fff;
+}
+footer {
+    text-align: center;
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
 }
