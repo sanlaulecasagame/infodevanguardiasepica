1a3642f98fdaf7a989108d2c48403decd0a40ca0
sanlaulecasagame <121693318+sanlaulecasagame@users.noreply.github.com>
2023-10-24 23:57:23 -0300

    Update styles.cs

--- styles.cs
+++ styles.cs
@@ -1,55 +1,62 @@
 /* styles.css */
 body {
-        background-image: url('fondo.jpg');
+    background-image: url('fondo.jpg');
     background-size: cover;
     background-repeat: no-repeat;
-    background-position: center;        
+    background-position: center;
     font-family: 'Super Sedan', sans-serif;
     margin: 0;
     padding: 0;
     background-color: #000000;
+    color: #fff;
+    text-align: center;
 }
+
 h1, h2, h3, p {
     font-family: 'Super Sedan', sans-serif;
 }
+
 .centrado {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
-}  
+    color: #fff;
+}
+
 @font-face {
     font-family: 'Super Sedan';
     src: url('SuperSedan.ttf') format('truetype');
 }
+
 header {
     background-color: #333;
-    color: #fff;
-    text-align: center;
     padding: 20px;
 }
+
 nav ul {
     list-style: none;
     padding: 0;
-    text-align: center;
 }
+
 nav li {
     display: inline;
     margin: 0 15px;
 }
+
 nav a {
     text-decoration: none;
 }
+
 main {
     max-width: 800px;
     margin: 20px auto;
     padding: 20px;
     background-color: #000;
 }
+
 footer {
-    text-align: center;
     background-color: #333;
-    color: #fff;
     padding: 10px;
 }
